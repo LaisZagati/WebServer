b63a3803cbed6ade96fc18af65f0bcc94588532a
LaisZagati <laisviana97@gmail.com>
2024-11-10 16:05:09 +0000

    Linked file html to py.server and created new about.html

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+
+
 
